0f2469c39cfa68ea252304524942b2f682c77ce6
FM-HXR <78189593+FM-HXR@users.noreply.github.com>
2021-04-07 17:06:24 +0900

    Update topic_serializer.rb

--- app/serializers/topic_serializer.rb
+++ app/serializers/topic_serializer.rb
@@ -1,6 +1,6 @@
 class TopicSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :title, :description, :pro_con, :user
+  attributes :title, :description, :pro, :con, :user
 
   belongs_to :user
   has_many :points
