987b3a8b4ad5d13a6098a1a59f70165892ee20ca
FM-HXR <78189593+FM-HXR@users.noreply.github.com>
2021-04-13 15:44:53 +0900

    Show Point (post comment) GUI

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,6 +1,6 @@
 class CommentSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :text, :point, :user
+  attributes :text, :point, :user, :created_at, :updated_at
 
   belongs_to :point
   belongs_to :user
