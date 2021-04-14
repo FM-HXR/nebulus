2fe6c5ce82d1808f53e64475ad442ba99b85f622
FM-HXR <78189593+FM-HXR@users.noreply.github.com>
2021-04-14 14:35:08 +0900

    Show Topic GUI ++
    Added New Topic Form Interfacing
    Added Markdown Preview on form
    Added Character Counter
    Correctly implemented fontawesome
    Changed Color scheme to black-white

--- app/javascript/components/Topic/Topic.js
+++ app/javascript/components/Topic/Topic.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Link } from "react-router-dom";
-import FontAwesome from "react-fontawesome";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlusSquare, faTimes } from "@fortawesome/free-solid-svg-icons";
 import axios from "axios";
+import ReactMarkdown from "react-markdown";
+import gfm from "remark-gfm";
 import LoadingBar from "react-top-loading-bar";
 
 const Topic = (props) => {
@@ -9,7 +12,9 @@ const Topic = (props) => {
   const [points, setPoints] = useState([]);
   const [point, setPoint] = useState({});
   const [loaded, setLoaded] = useState(false);
+  const [formOpen, setFormOpen] = useState(false);
   const [progress, setProgress] = useState(0);
+
   const userPresence = document.querySelector(".user-presence");
   let loginStatus = userPresence.getAttribute("data-user");
   // console.log("Props: ", props);
@@ -127,9 +132,84 @@ const Topic = (props) => {
 
         titleField.value = "";
         argumentField.value = "";
+
+        const newId = resp.data.data.id;
+        window.location = `/points/${newId}`;
       })
       .catch((resp) => console.log(resp));
   };
+
+  const showForm = (e) => {
+    e.preventDefault();
+
+    const plus = document.querySelector(".add-point");
+    const formContent = document.getElementById("form-content");
+    const title = document.querySelector(".topic-title");
+    const desc = document.querySelector(".topic-description");
+    const back = document.querySelector(".return-form");
+    const close = document.querySelector(".close-form");
+
+    plus.style.display = "none";
+    title.style.display = "none";
+    desc.style.display = "none";
+    back.style.display = "none";
+    close.style.display = "none";
+
+    formContent.style.display = "grid";
+    formContent.classList.add("formContent");
+    setFormOpen(true);
+    console.log("Show Ok");
+  };
+
+  const hideForm = (e) => {
+    e.preventDefault();
+
+    const back = document.querySelector(".return-form");
+    const close = document.querySelector(".close-form");
+    const formContent = document.getElementById("form-content");
+    const title = document.querySelector(".topic-title");
+    const desc = document.querySelector(".topic-description");
+
+    title.style.display = "";
+    desc.style.display = "";
+    back.style.display = "flex";
+    close.style.display = "flex";
+    formContent.classList.remove("formContent");
+    formContent.style.display = "none";
+    console.log("Hide Ok");
+  };
+
+  const closeForm = (e) => {
+    const close = document.querySelector(".close-form");
+    const back = document.querySelector(".return-form");
+    const titleField = document.getElementById("title");
+    const argumentField = document.getElementById("argument");
+    const plus = document.querySelector(".add-point");
+
+    setFormOpen(true);
+    titleField.value = "";
+    argumentField.value = "";
+    plus.style.display = "flex";
+    back.style.display = "none";
+    close.style.display = "none";
+  };
+
+  const openPreview = (e) => {
+    e.preventDefault();
+    const preview = document.querySelector(".preview-shell");
+    console.log(preview);
+    preview.style.display = "flex";
+  };
+
+  const closePreview = (e) => {
+    e.preventDefault();
+    const preview = document.querySelector(".preview-shell");
+    console.log(preview);
+    preview.style.display = "none";
+  };
+
+  const charCountTit = document.getElementById("title");
+  const charCountArg = document.getElementById("argument");
 
   return (
     loaded && (
@@ -147,13 +227,21 @@ const Topic = (props) => {
           <p className="topic-description">
             {topic.data.attributes.description}
           </p>
+          <button className="return-form" onClick={showForm}>
+            Show Form
+          </button>
+          <button className="close-form" onClick={closeForm}>
+            Close Form
+          </button>
         </div>
 
-        <FontAwesome
-          name="plus-square"
-          // onClick={handleClickOne}
-          className="add-point"
-        />
+        {loginStatus === "true" && (
+          <FontAwesomeIcon
+            icon={faPlusSquare}
+            onClick={showForm}
+            className="add-point"
+          />
+        )}
 
         <div className="pro-shell">
           <p className="pro">{topic.data.attributes.pro}</p>
@@ -165,59 +253,81 @@ const Topic = (props) => {
           <div className="con-container">{cons}</div>
         </div>
 
-        <div className="point-form-shell">
-          <form className="point-form" onSubmit={handleSubmit}>
-            <textarea
-              name="title"
-              maxLength="150"
-              placeholder="Point Title: Max Char Length 150"
+        {/* Point Form */}
+        <form className="point-form" onSubmit={handleSubmit} id="form-content">
+          <textarea
+            name="title"
+            maxLength="150"
+            placeholder="Point Title: Max Char Length 150"
+            onChange={handleChange}
+            className="point-form-title"
+            id="title"
+          />
+
+          <div className="point-form-position">
+            <p>Argue For:</p>
+            <select
+              name="position"
               onChange={handleChange}
-              className="point-form-title"
-              id="title"
-            />
+              className="select-position"
+              id="select-p"
+            >
+              <option value={null}>--</option>
+              <option value={true}>{topic.data.attributes.pro}</option>
+              <option value={false}>{topic.data.attributes.con}</option>
+            </select>
+          </div>
 
-            <div className="point-form-position">
-              <p>Argue For:</p>
-              <select
-                name="position"
-                onChange={handleChange}
-                className="select-position"
-                id="select-p"
-              >
-                <option value={null}>--</option>
-                <option value={true}>{topic.data.attributes.pro}</option>
-                <option value={false}>{topic.data.attributes.con}</option>
-              </select>
-            </div>
+          <button className="see-desc" onClick={hideForm}>
+            Hide Form
+          </button>
 
-            <button className="see-desc">See Topic Description</button>
-
-            <div className="point-form-format">
-              <p>Format</p>
-              <select
-                name="markdown"
-                onChange={handleChange}
-                className="select-format"
-                id="select-f"
-              >
-                <option value={null}>--</option>
-                <option value={true}>Markdown</option>
-                <option value={false}>Text</option>
-              </select>
-            </div>
-
-            <textarea
-              name="argument"
-              maxLength="2000"
-              placeholder="Max Char Length 2000"
+          <div className="point-form-format">
+            <p>Format</p>
+            <select
+              name="markdown"
               onChange={handleChange}
-              className="point-form-argument"
-              id="argument"
-            />
-            <button type="submit" className="point-submit">
-              Post
-            </button>
-          </form>
+              className="select-format"
+              id="select-f"
+            >
+              <option value={null}>--</option>
+              <option value={true}>Markdown</option>
+              <option value={false}>Text</option>
+            </select>
+          </div>
+
+          <textarea
+            name="argument"
+            maxLength="5000"
+            placeholder="Max Char Length 5000"
+            onChange={handleChange}
+            className="point-form-argument"
+            id="argument"
+          />
+          <ul className="point-field-counts">
+            <li>
+              Title: {formOpen && 150 - charCountTit.value.length} Char Left
+            </li>
+            <li>
+              Argument: {formOpen && 5000 - charCountArg.value.length} Char Left
+            </li>
+            <button onClick={openPreview}>Markdown Preview</button>
+          </ul>
+          <button type="submit" className="point-submit">
+            Post
+          </button>
+        </form>
+        <div className="preview-shell">
+          <FontAwesomeIcon
+            icon={faTimes}
+            onClick={closePreview}
+            className="close-preview"
+          />
+          {formOpen && (
+            <ReactMarkdown plugins={[gfm]} className="point-preview">
+              {charCountArg.value}
+            </ReactMarkdown>
+          )}
         </div>
       </div>
     )
